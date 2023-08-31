923cdd7db1464a1234fa3b5efce69294abe35f7b
mahdi changizi <mchangizi10@gmail.com>
2023-08-31 10:32:28 +0330

    create models acl and relashionship

---
+++ app/Models/Admin/Permission.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models\Admin;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Permission extends Model
+{
+    use HasFactory;
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
+}

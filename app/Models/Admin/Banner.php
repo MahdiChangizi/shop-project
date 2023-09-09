3ec19f58c78ac1051b220257336083690cdbc99f
mahdi changizi <mchangizi10@gmail.com>
2023-09-09 21:01:21 +0330

    add migration and model

---
+++ app/Models/Admin/Banner.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\Admin;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Banner extends Model
+{
+    use HasFactory;
+    protected $fillable = ['url', 'image'];
+
+}

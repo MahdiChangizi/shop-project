c2633f6c442b639d2cf837554c40b19b63146e52
mahdi changizi <mchangizi10@gmail.com>
2023-09-22 00:09:38 +0330

    modify Request Permission

--- app/Http/Requests/Admin/Permission/PermissionStoreRequest.php
+++ app/Http/Requests/Admin/Permission/PermissionStoreRequest.php
@@ -11,7 +11,7 @@ class PermissionStoreRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,7 @@ class PermissionStoreRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required', 'min:5', 'max:20']
         ];
     }
 }

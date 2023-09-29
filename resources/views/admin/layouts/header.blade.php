e4d63e1f478b450a1ba1b71ada02d2487eb0fcc7
mahdi changizi <mchangizi10@gmail.com>
2023-09-29 18:32:44 +0330

    modify profile user

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -1,74 +1,7 @@
 <nav class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme"
     id="layout-navbar">
 
-    {{-- <li class="nav-item dropdown-notifications navbar-dropdown dropdown me-3 me-xl-1 list-unstyled">
-
-        <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false">
-          <i class="ti ti-bell ti-md"></i>
-          <span class="badge bg-danger rounded-pill badge-notifications">5</span>
-        </a>
-
-        <ul class="dropdown-menu dropdown-menu-end py-0">
-
-          <li class="dropdown-menu-header border-bottom">
-            <div class="dropdown-header d-flex align-items-center py-3">
-              <h5 class="text-body mb-0 me-auto">Notification</h5>
-              <a href="javascript:void(0)" class="dropdown-notifications-all text-body" data-bs-toggle="tooltip" data-bs-placement="top" aria-label="Mark all as read" data-bs-original-title="Mark all as read"><i class="ti ti-mail-opened fs-4"></i></a>
-            </div>
-          </li>
-
-            <ul class="list-group list-group-flush">
-              
-              <li class="list-group-item list-group-item-action dropdown-notifications-item marked-as-read">
-                <div class="d-flex">
-                  <div class="flex-shrink-0 me-3">
-                    <div class="avatar">
-                      <img src="../../assets/img/avatars/9.png" alt="" class="h-auto rounded-circle">
-                    </div>
-                  </div>
-                  <div class="flex-grow-1">
-                    <h6 class="mb-1">Application has been approved 🚀</h6>
-                    <p class="mb-0">Your ABC project application has been approved.</p>
-                    <small class="text-muted">2 days ago</small>
-                  </div>
-                  <div class="flex-shrink-0 dropdown-notifications-actions">
-                    <a href="javascript:void(0)" class="dropdown-notifications-read"><span class="badge badge-dot"></span></a>
-                    <a href="javascript:void(0)" class="dropdown-notifications-archive"><span class="ti ti-x"></span></a>
-                  </div>
-                </div>
-              </li>
-            </ul>
-
-          <div class="ps__rail-x" style="left: 0px; bottom: 0px;"><div class="ps__thumb-x" tabindex="0" style="left: 0px; width: 0px;"></div></div><div class="ps__rail-y" style="top: 0px; right: 0px;"><div class="ps__thumb-y" tabindex="0" style="top: 0px; height: 0px;"></div></div></li>
-          <li class="dropdown-menu-footer border-top">
-            <a href="javascript:void(0);" class="dropdown-item d-flex justify-content-center text-primary p-2 h-px-40 mb-1 align-items-center">
-              View all notifications
-            </a>
-          </li>
-        </ul>
-
-    </li> --}}
     
-    <li class="nav-item dropdown-notifications navbar-dropdown dropdown me-3 me-xl-1 list-unstyled">
-        <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false">
-            <i class="ti ti-bell ti-md"></i>
-            <span class="badge bg-danger rounded-pill badge-notifications">5</span>
-        </a>
-    
-        <ul class="dropdown-menu dropdown-menu-end py-0">
-            <li class="dropdown-menu-header border-bottom">
-                <div class="dropdown-header d-flex align-items-center py-3">
-                    <h5 class="text-body mb-0 me-auto">Notification</h5>
-                    <a href="javascript:void(0)" class="dropdown-notifications-all text-body" data-bs-toggle="tooltip" data-bs-placement="top" aria-label="Mark all as read" data-bs-original-title="Mark all as read">
-                        <i class="ti ti-mail-opened fs-4"></i>
-                    </a>
-                </div>
-            </li>
-            <!-- محتوای منو -->
-        </ul>
-    </li>
-    
-
     <div class="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none">
         <a class="nav-item nav-link px-0 me-xl-4" href="javascript:void(0)">
             <i class="ti ti-menu-2 ti-sm"></i>
@@ -86,6 +19,53 @@
             </li>
             <!--/ Style Switcher -->
 
+            <li class="nav-item dropdown-notifications navbar-dropdown dropdown me-3 me-xl-1 list-unstyled">
+
+                <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false">
+                  <i class="ti ti-bell ti-md"></i>
+                  <span class="badge bg-danger rounded-pill badge-notifications">5</span>
+                </a>
+        
+                <ul class="dropdown-menu dropdown-menu-end py-0">
+        
+                  <li class="dropdown-menu-header border-bottom">
+                    <div class="dropdown-header d-flex align-items-center py-3">
+                      <h5 class="text-body mb-0 me-auto">Notification</h5>
+                      <a href="javascript:void(0)" class="dropdown-notifications-all text-body" data-bs-toggle="tooltip" data-bs-placement="top" aria-label="Mark all as read" data-bs-original-title="Mark all as read"><i class="ti ti-mail-opened fs-4"></i></a>
+                    </div>
+                  </li>
+        
+                    <ul class="list-group list-group-flush">
+                      
+                      <li class="list-group-item list-group-item-action dropdown-notifications-item marked-as-read">
+                        <div class="d-flex">
+                          <div class="flex-shrink-0 me-3">
+                            <div class="avatar">
+                              <img src="../../assets/img/avatars/9.png" alt="" class="h-auto rounded-circle">
+                            </div>
+                          </div>
+                          <div class="flex-grow-1">
+                            <h6 class="mb-1">Application has been approved 🚀</h6>
+                            <p class="mb-0">Your ABC project application has been approved.</p>
+                            <small class="text-muted">2 days ago</small>
+                          </div>
+                          <div class="flex-shrink-0 dropdown-notifications-actions">
+                            <a href="javascript:void(0)" class="dropdown-notifications-read"><span class="badge badge-dot"></span></a>
+                            <a href="javascript:void(0)" class="dropdown-notifications-archive"><span class="ti ti-x"></span></a>
+                          </div>
+                        </div>
+                      </li>
+                    </ul>
+        
+                  <div class="ps__rail-x" style="left: 0px; bottom: 0px;"><div class="ps__thumb-x" tabindex="0" style="left: 0px; width: 0px;"></div></div><div class="ps__rail-y" style="top: 0px; right: 0px;"><div class="ps__thumb-y" tabindex="0" style="top: 0px; height: 0px;"></div></div></li>
+                  <li class="dropdown-menu-footer border-top">
+                    <a href="javascript:void(0);" class="dropdown-item d-flex justify-content-center text-primary p-2 h-px-40 mb-1 align-items-center">
+                      View all notifications
+                    </a>
+                  </li>
+                </ul>
+        
+            </li>
 
             <!-- User -->
             <li class="nav-item navbar-dropdown dropdown-user dropdown">

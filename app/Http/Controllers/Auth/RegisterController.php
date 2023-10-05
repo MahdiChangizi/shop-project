b4184bdb481247b4543d0eabb4983890f41619ed
mahdichangizi <mchangizi10@gmail.com>
2023-10-05 09:17:00 -0400

    edit code

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Models\Admin\ActiveCode;
+use App\Models\User;
+use App\Notifications\CodeNotification;
+use Illuminate\Support\Facades\Auth;
+
+class RegisterController extends Controller
+{
+    public function registerForm()
+    {
+        return view('auth.register');
+    }
+
+    public function register(RegisterRequest $request)
+    {
+        /* validateion */
+        $inputs = $request->all();
+
+        /* create user */
+        $user = User::create($inputs);
+        if($user)
+        {
+            Auth::loginUsingId($user->id);
+        }
+
+        /* generate code */
+        $code = ActiveCode::generateCode($user);
+
+        /* csend code to user */
+        $request->user()->notify(new CodeNotification($code, $user['mobile']));
+
+        if($user['mobile_verified_at'] == null)
+        {
+            return to_route('auth.activationForm');
+        } else  {
+            return to_route('coustomer.profile');
+        }
+    }
+}
